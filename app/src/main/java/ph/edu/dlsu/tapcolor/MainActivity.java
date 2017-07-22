e9c66647a7984a2cf7acc4b8ed4e2b6f3e857c77
melvincabatuan <melvincabatuan@gmail.com>
2017-07-22 15:05:30 +0800

    first commit

---
+++ app/src/main/java/ph/edu/dlsu/tapcolor/MainActivity.java
@@ -0,0 +1,147 @@
+package ph.edu.dlsu.tapcolor;
+
+import android.app.Activity;
+import android.graphics.Color;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.MotionEvent;
+import android.view.SurfaceView;
+import android.view.View;
+import android.view.WindowManager;
+import android.widget.TextView;
+
+import org.opencv.android.CameraBridgeViewBase;
+import org.opencv.core.Core;
+import org.opencv.core.CvType;
+import org.opencv.core.Mat;
+import org.opencv.core.Rect;
+import org.opencv.core.Scalar;
+import org.opencv.imgproc.Imgproc;
+
+public class MainActivity extends Activity implements View.OnTouchListener,CameraBridgeViewBase.CvCameraViewListener2 {
+    private static final String TAG = "OpenCV::CameraActivity";
+
+    private CameraBridgeViewBase mOpenCvCameraView;
+    private Mat mRgba;
+
+    private Scalar mBlobColorHsv;
+    private Scalar mBlobColorRgba;
+
+    double x = -1;
+    double y = -1;
+    TextView touch_coordinates;
+    TextView touch_color;
+
+    static {
+        System.loadLibrary("opencv_java310"); // libopencv_java3.so located at jniLibs directory
+    }
+
+    /**
+     * Called when the activity is first created.
+     */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        Log.i(TAG, "called onCreate");
+        super.onCreate(savedInstanceState);
+        getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
+        setContentView(R.layout.activity_main);
+
+        touch_coordinates = (TextView) findViewById(R.id.touch_coordinates);
+        touch_color = (TextView) findViewById(R.id.touch_color);
+
+        mOpenCvCameraView = (CameraBridgeViewBase) findViewById(R.id.main_activity_java_surface_view);
+        mOpenCvCameraView.setVisibility(SurfaceView.VISIBLE);
+        mOpenCvCameraView.setCvCameraViewListener(this);
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        if (mOpenCvCameraView != null)
+            mOpenCvCameraView.disableView();
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        mOpenCvCameraView.enableView();
+    }
+
+    public void onDestroy() {
+        super.onDestroy();
+        if (mOpenCvCameraView != null)
+            mOpenCvCameraView.disableView();
+    }
+
+    public void onCameraViewStarted(int width, int height) {
+        mRgba = new Mat();
+        mBlobColorRgba = new Scalar(255);
+        mBlobColorHsv = new Scalar(255);
+    }
+
+    public void onCameraViewStopped() {
+        mRgba.release();
+    }
+
+    public Mat onCameraFrame(CameraBridgeViewBase.CvCameraViewFrame inputFrame) {
+        mRgba = inputFrame.rgba();
+        return mRgba;
+    }
+
+    @Override
+    public boolean onTouch(View v, MotionEvent event) {
+        int cols = mRgba.cols();
+        int rows = mRgba.rows();
+        double yLow = (double)mOpenCvCameraView.getHeight() * 0.2401961;
+        double yHigh = (double)mOpenCvCameraView.getHeight() * 0.7696078;
+        double xScale = (double)cols / (double)mOpenCvCameraView.getWidth();
+        double yScale = (double)rows / (yHigh - yLow);
+        x = event.getX();
+        y = event.getY();
+        y = y - yLow;
+        x = x * xScale;
+        y = y * yScale;
+        if((x < 0) || (y < 0) || (x > cols) || (y > rows)) return false;
+        touch_coordinates.setText("X: " + Double.valueOf(x) + ", Y: " + Double.valueOf(y));
+        Rect touchedRect = new Rect();
+
+        touchedRect.x = (int)x;
+        touchedRect.y = (int)y;
+
+        touchedRect.width = 8;
+        touchedRect.height = 8;
+
+        Mat touchedRegionRgba = mRgba.submat(touchedRect);
+
+        Mat touchedRegionHsv = new Mat();
+        Imgproc.cvtColor(touchedRegionRgba, touchedRegionHsv, Imgproc.COLOR_RGB2HSV_FULL);
+        Imgproc.cvtColor(touchedRegionRgba, touchedRegionHsv, Imgproc.COLOR_RGB2HSV_FULL);
+
+        mBlobColorHsv = Core.sumElems(touchedRegionHsv);
+        int pointCount = touchedRect.width * touchedRect.height;
+        for (int i = 0; i < mBlobColorHsv.val.length; i++)
+            mBlobColorHsv.val[i] /= pointCount;
+
+        mBlobColorRgba = convertScalarHsv2Rgba(mBlobColorHsv);
+
+        touch_color.setText("Color: #" + String.format("%02X", (int)mBlobColorRgba.val[0])
+                + String.format("%02X", (int)mBlobColorRgba.val[1])
+                + String.format("%02X", (int)mBlobColorRgba.val[2]));
+
+        touch_color.setTextColor(Color.rgb((int) mBlobColorRgba.val[0],
+                (int) mBlobColorRgba.val[1],
+                (int) mBlobColorRgba.val[2]));
+        touch_coordinates.setTextColor(Color.rgb((int)mBlobColorRgba.val[0],
+                (int)mBlobColorRgba.val[1],
+                (int)mBlobColorRgba.val[2]));
+        return false;
+    }
+
+    private Scalar convertScalarHsv2Rgba(Scalar hsvColor) {
+        Mat pointMatRgba = new Mat();
+        Mat pointMatHsv = new Mat(1, 1, CvType.CV_8UC3, hsvColor);
+        Imgproc.cvtColor(pointMatHsv, pointMatRgba, Imgproc.COLOR_HSV2RGB_FULL, 4);
+
+        return new Scalar(pointMatRgba.get(0, 0));
+    }
+}
